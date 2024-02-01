fefc293de11f519dfa496ed4debda6e7d25f3946
AnnaKhachatryan1 <157060561+AnnaKhachatryan1@users.noreply.github.com>
2024-02-01 22:33:08 +0400

    FIX Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,33 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text(
+          selectedIndex == 0 ? "Add Note" : "All Notes",
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        selectedFontSize: 20 ,
+        unselectedItemColor: Colors.black87,
+        unselectedFontSize: 14,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          currentIndex: selectedIndex,
+          onTap: (value) {
+            selectedIndex = value;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(icon: Container(), label: "Add Note"),
+            BottomNavigationBarItem(icon: Container(), label: "All Notes"),
+          ]),
+          
+          body: selectedIndex == 0 ?  myAddNote(): myAllNotes(),
     );
+  }
+  
+
+  Widget myAddNote(){
+    return Container(color: Colors.yellow, child: Center( child: Text("Hello"),),) ;
+  }
+
+  Widget myAllNotes(){
+    return Container(color: Colors.green, child: Center( child: Text("Hy"),),) ;
   }
 }
