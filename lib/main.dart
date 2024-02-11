7738af471319e8559e9f26ab7d723dff471e256c
AnnaKhachatryan1 <157060561+AnnaKhachatryan1@users.noreply.github.com>
2024-02-11 22:07:26 +0400

    FIX using TextController

--- lib/main.dart
+++ lib/main.dart
@@ -30,82 +30,99 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String tempNote = "";
   int selectedIndex = 0;
-  List<String> allNotes = ["aaa", "111"];
+  List<String> allNotes = [];
+  TextEditingController notesController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        centerTitle: true,
-        title: Text(
-          selectedIndex == 0 ? "Add Note" : "All Notes",
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.black,
-          selectedFontSize: 20,
-          unselectedItemColor: Colors.black87,
-          unselectedFontSize: 14,
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          currentIndex: selectedIndex,
-          onTap: (value) {
-            selectedIndex = value;
-            setState(() {});
-          },
-          items: [
-            BottomNavigationBarItem(icon: Container(), label: "Add Note"),
-            BottomNavigationBarItem(icon: Container(), label: "All Notes"),
-          ]),
-      body: selectedIndex == 0 ? myAddNote() : myAllNotes(),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            centerTitle: true,
+            title: Text(
+              selectedIndex == 0 ? "Add Note" : "All Notes",
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              selectedItemColor: Colors.black,
+              selectedFontSize: 20,
+              unselectedItemColor: Colors.black87,
+              unselectedFontSize: 14,
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              currentIndex: selectedIndex,
+              onTap: (value) {
+                selectedIndex = value;
+                setState(() {});
+              },
+              items: [
+                BottomNavigationBarItem(icon: Container(), label: "Add Note"),
+                BottomNavigationBarItem(icon: Container(), label: "All Notes"),
+              ]),
+          body: GestureDetector(
+            onTap: () {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            child: selectedIndex == 0 ? myAddNote() : myAllNotes(),
+          )),
     );
   }
 
   Widget myAddNote() {
-    return Center(
-        child: Container(
-            padding: EdgeInsets.all(20),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      decoration: BoxDecoration(border: Border.all()),
-                      child: TextFormField(
-                        maxLines: 5,
-                        decoration: InputDecoration(border: InputBorder.none),
-                        onChanged: (value) {
-                          tempNote = value;
-                          setState(() {});
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Center(
+          child: Container(
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        child: TextFormField(
+                          controller: notesController,
+                          maxLines: 5,
+                          decoration: InputDecoration(border: InputBorder.none),
+                          // onChanged: (value) {
+                          //   tempNote = value;
+                          //   setState(() {});
+                          // },
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          allNotes.add(notesController.text);
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          notesController.value = TextEditingValue.empty;
+                          print(allNotes);
                         },
-                      )),
-                  TextButton(
-                      onPressed: () {
-                        allNotes.add(tempNote);
-                        print(allNotes);
-                      },
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        child: Text(
-                          "Add",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ))
-                ],
-              ),
-            )));
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          child: Text(
+                            "Add",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ))
+                  ],
+                ),
+              ))),
+    );
   }
 
   Widget myAllNotes() {
     List<Widget> widgets = allNotes.mapIndexed((index, note) {
       return Container(
           height: 50,
-          color: index % 2 == 0 ? Colors.grey : Colors.blueGrey,
+          color: index % 2 == 0 ? Colors.purple : Colors.purpleAccent,
           child: Text(note));
     }).toList();
-
 
     return Container(child: ListView(children: widgets));
   }
